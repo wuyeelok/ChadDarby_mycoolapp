9a868a65d0f0de827f96ebcd629d92039831c99a
wuyeelok <wuyeelok@gmail.com>
2019-11-28 11:09:30 +0800

    Custom defined prop value

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,11 +2,28 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+	// Inject properties for: coach.name and team.name
+	private final String coachName;
+	private final String teamName;
+
+	public FunRestController(@Value("${coach.name}") String coachName, @Value("${team.name}") String teamName) {
+		this.coachName = coachName;
+		this.teamName = teamName;
+	}
+
+	// expose new endpoint for "teaminfo"
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + this.coachName + ", Team name: " + this.teamName;
+	}
 
 	// expose "/" that return "Hello World"
 
